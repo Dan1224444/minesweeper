440b3f884bf9ebcd172449e7b209c63322d6bb01
Daniel Li <danielli12357@gmail.com>
2020-12-28 00:00:47 +1300

    [Add] onclick function to print the number of adjacent bombs

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 document.addEventListener('DOMContentLoaded', () => {
     const gameBoard = document.querySelector('.game-board');
     let width = 10;
+    let height = 10;
     //stores the location of the bombs
     let bombPlots = [];
 
     function createBoard() {
-        for (let i = 0; i < width; i++) {
+        for (let i = 0; i < height; i++) {
             for (let j = 0; j < width; j++) {
             const plot = document.createElement('div');
-            plot.setAttribute('id', `${i}-${j}`);
+            plot.addEventListener('click',function(e) {
+                clicked(j,i);
+            })
+            plot.setAttribute('id', `${j}-${i}`);
             gameBoard.appendChild(plot);
             }
         }
@@ -16,18 +20,68 @@ document.addEventListener('DOMContentLoaded', () => {
 
     function fillPlots(noBombs){
         for (let i = 0; i < noBombs; i++){
-            let coord = Math.floor(Math.random() * 100);
-            let xCoord = Math.floor(coord/10);
-            let yCoord = coord % 10;
+            let xCoord = Math.floor(Math.random() * width);
+            let yCoord = Math.floor(Math.random() * height);
 
             bombPlots.push([xCoord,yCoord]);
         }
     }
 
-    function clicked(plot){
-        
+    function gameOver(){
+        alert("game voer");
+    }
+
+    function isBomb(xCoord, yCoord){
+        let flag = false;
+        bombPlots.forEach(plot => {
+            if (plot[0] == xCoord && plot[1] == yCoord) {
+                flag = true;
+            }
+        });
+        return flag;
+    }
+
+    function clicked(xCoord, yCoord){
+        let bound = [];
+        let yBound = [];
+        let count =0;
+
+        console.log("Clicked" , xCoord, yCoord);
+
+        if (isBomb(xCoord, yCoord)){
+            gameOver();
+        } else {
+            if (xCoord == 0){
+                bound.push([0, 1]);
+            } else if (xCoord == width -1 ) {
+                bound.push([-1, 0]);
+            } else {
+                bound.push([-1, 1]);
+            }
+
+            if (yCoord == 0){
+                bound.push([0, 1]);
+            } else if (yCoord == height-1 ) {
+                bound.push([-1, 0]);
+            } else {
+                bound.push([-1, 1]);
+            }
+            
+           for(let i = bound[0][0]; i <= bound[0][1]; i++ ){
+               for (let j = bound[1][0]; j <= bound[1][1]; j++) {
+                    if (isBomb(xCoord + i, yCoord + j)) {
+                        count ++;
+                    }
+               }
+           }
+
+           const plot = document.getElementById(`${xCoord}-${yCoord}`);
+           plot.innerHTML = `${count}`;
+            
+        }
 
     }
     createBoard();
     fillPlots(5);
+    console.log(bombPlots);
 })
